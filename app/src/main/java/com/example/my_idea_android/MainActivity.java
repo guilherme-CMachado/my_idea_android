9059a10014107451114415c79aa23873daa7b5ad
Guilherme Machado <guicastro2812@gmail.com>
2022-09-20 15:35:24 -0300

    Finalização 2 telas minha_Ideia

--- app/src/main/java/com/example/my_idea_android/MainActivity.java
+++ app/src/main/java/com/example/my_idea_android/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.my_idea_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "My_Idea_App";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: tela_principal_carregada");
     }
 }
--- app/src/main/java/com/example/my_idea_android/SplashActivity.java
+++ app/src/main/java/com/example/my_idea_android/SplashActivity.java
@@ -2,13 +2,37 @@ package com.example.my_idea_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
+    
+    String TAG = "My_Idea_App";
+    int tempoDeEspera = 1000 * 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d(TAG, "onCreate: Splash_Screen_Carregada");
+        trocarTela();
+    }
+
+    private void trocarTela(){
+
+        Log.d(TAG, "trocarTela: trocando de tela...");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent trocarTela = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(trocarTela);
+                finish();
+            }
+        },tempoDeEspera);
+
+
     }
 }
